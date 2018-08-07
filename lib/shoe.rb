c6c2fbc67a422ef40570128d25f2ef3de4f355e9
github username <github email address>
2018-08-07 13:00:20 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,11 +4,17 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
+
+  BRANDS = []
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+
 
 end
